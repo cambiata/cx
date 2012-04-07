2107c96e9b97a235a80e3f0486f3038f9fb2f0c0
Jonas Nystrom <jonasnys@gmail.com>
2012-04-07 20:03:23 +0200

    update

--- src/Cx.hx
+++ src/Cx.hx
@@ -1,3 +1,4 @@
+import neko.FileSystem;
 /**
  * ...
  * @author Jonas Nyström
@@ -231,5 +232,92 @@ class Cx {
 		putContentBinary(pngFileName, byteArray.asString());
 	}
 	
+	//----------------------------------------------------------------------------------------------------------
+	
+	static public function filesNamesInDirectory(dir:String) {
+		var filenames = neko.FileSystem.readDirectory(dir);
+		filenames.sort(function(a, b) return Reflect.compare(a.toLowerCase(), b.toLowerCase()) );		
+		return filenames;
+	}
+	
+	static public function filesBytesInDirectory(dir:String):List<haxe.io.Bytes> {
+		var bytesList = new List<haxe.io.Bytes>();
+		var filenames = filesNamesInDirectory(dir);
+		for (filename in filenames) { bytesList.add(neko.io.File.getBytes(dir + filename)); }		
+		return bytesList;
+	}
+	
+	//----------------------------------------------------------------------------------------------------------
+	/*
+	static public function filesegmentsTest() {
+		trace(Cx.filesNamesInDirectory('files'));
+		var bytesList = Cx.filesBytesInDirectory('files/');
+		var bytes = Cx.filesegmentsJoinBytes(bytesList);
+		Cx.putContentBinary('join.files', bytes.toString());
+		var bytesList2 = Cx.filesegmentsSplitBytes(bytes, bytesList.length);
+		
+		var i = 0;
+		for (bytes in bytesList2) {
+			Cx.putContentBinary('file-' + i + '.file', bytes.toString());
+			i++;
+		}	
+	}	
+	*/
+	public static function filesegmentsJoinBytes(bytesList:List<haxe.io.Bytes>, ?segmentLength:Int = 4):haxe.io.Bytes {
+		bytesList = filesegmentsFillUp(bytesList, segmentLength);
+		var channelsLength:Int = bytesList.length;
+		var bytesLength = bytesList.first().length;
+		var totalLength = channelsLength * bytesLength;		
+		var totalSegmentsCount = Std.int(totalLength / segmentLength);
+		var totalIterations:Int = Std.int(totalSegmentsCount / channelsLength);
+		var resultBB = new haxe.io.BytesBuffer();
+		for (pos in 0...totalIterations) {
+			for (bytes in bytesList) {
+				var sub = bytes.sub(pos * segmentLength, segmentLength);
+				resultBB.add(sub);
+			}
+		}		
+		return return resultBB.getBytes();
+	}
+
+	public static function filesegmentsFillUp(bytesList:List<haxe.io.Bytes>, ?segmentLength:Int = 4):List<haxe.io.Bytes> {
+		var longest:Float = 0;			
+		bytesList.map(function(bytes:haxe.io.Bytes):Void { longest = Math.max(bytes.length, longest); } ); 
+		var nrOfSegments  = Math.ceil(longest / segmentLength);
+		var filledBytesLength = nrOfSegments * segmentLength;
+		var result = new List<haxe.io.Bytes>();
+		for (bytes in bytesList) {
+			var fillCount = filledBytesLength - bytes.length;
+			var fillString = StringTools.lpad('', '0', fillCount);
+			var bb = new haxe.io.BytesBuffer();
+			bb.add(bytes);
+			bb.add(haxe.io.Bytes.ofString(fillString)); 
+			var newBytes = bb.getBytes();
+			result.add(newBytes);
+		}
+		return result;
+	}	
+	
+	static public function filesegmentsSplitBytes(bytes:haxe.io.Bytes, nrOfChannels:Int, segmentsLength:Int=4):List<haxe.io.Bytes> {
+		var iterations = Std.int(bytes.length / (segmentsLength * nrOfChannels));
+		var bbList = new Array<haxe.io.BytesBuffer>();
+		for (ch in 0...nrOfChannels) {
+			bbList.push(new haxe.io.BytesBuffer());
+		}
+		for (i in 0...iterations) {
+			for (ch in 0...nrOfChannels) {
+				var pos:Int = (i * nrOfChannels * segmentsLength) +  (ch * segmentsLength);
+				var sub:haxe.io.Bytes = bytes.sub(pos, segmentsLength);
+				var chPos:Int = i * segmentsLength;
+				bbList[ch].addBytes(bytes, pos, segmentsLength);
+			}
+		}
+		var ret = new List<haxe.io.Bytes>();
+		for (ch in 0...nrOfChannels) {
+			ret.add(bbList[ch].getBytes());
+		}
+		return ret;
+	}
+
 	
 }
