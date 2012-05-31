72c17f77599b9c58d245a1d596909fcb3f33494e
Jonas Nystrom <jonasnys@gmail.com>
2012-05-31 07:48:10 +0200

    update

--- src/cx/test/StrToolsTest.hx
+++ src/cx/test/StrToolsTest.hx
@@ -15,15 +15,12 @@ class StrToolsTest extends TestCase
 	public function testSimilar1() {		
 		assertEquals('jonas'.similarity('jonas'), 1);
 		assertEquals('jonas'.similarity('jonax'), 0.8);
-		assertEquals('jonas'.similarity('xonas'), 0.8);
-		
+		assertEquals('jonas'.similarity('xonas'), 0.8);		
 		assertEquals('jonas'.similarity('jonxx'), 0.6);
-		assertEquals('jonas'.similarity('xxnas'), 0.6);
-		
+		assertEquals('jonas'.similarity('xxnas'), 0.6);		
 		assertEquals('jonas'.similarity('jonaxs').round2(3), 0.883);
 		assertEquals('jonas'.similarity('jxonas').round2(3), 0.783);		
 		
-		//assertEquals(MathTools.round2(123.123456), 123.12);
 		
 	}
 	
