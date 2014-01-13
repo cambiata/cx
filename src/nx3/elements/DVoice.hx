1616a02d01ac340a4636a4cbbcad56f4cec590b6
Jonas Nystrom <jonasnys@gmail.com>
2014-01-13 20:15:32 +0100

    update

---
+++ src/nx3/elements/DVoice.hx
@@ -0,0 +1,19 @@
+package nx3.elements;
+
+/**
+ * ...
+ * @author Jonas Nyström
+ */
+class DVoice
+{
+
+	public function new(voice:NVoice) 
+	{
+		this.voice = voice;
+	}
+	
+	var voice:NVoice;
+	
+	
+	
+}
